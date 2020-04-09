140a479f05d46fff88dc1e3bb47c52ef336ab1b5
Cesar Eduardo <cortiz@picap.co>
2020-04-09 18:51:47 -0500

    ADD: Mongo connection

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 'use strict'
 
-const { graphql, buildSchema } = require('graphql')
+require('dotenv').config()
+const { makeExecutableSchema } = require('graphql-tools')
+const express = require('express')
+const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
+const app = express()
+const port = process.env.port || 3000
 
 //Schema
-const schema = buildSchema(`
-  type Query{
-    hello: String,
-    saludo: String
-  }
-`)
+const typeDefs =  readFileSync(
+  join(__dirname,'lib','schema.graphql'), 'utf-8'
+);
+const schema = makeExecutableSchema({typeDefs, resolvers})
 
-// Run Query Hello
-graphql(schema, '{ hello }').then((data) => {
-  console.log(data)
+app.use('/api', gqlMiddleware({
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
+}))
+
+app.listen(port, ()=>{
+  console.log(`Server is listening at http://localhost:${port}/api`)
 })
